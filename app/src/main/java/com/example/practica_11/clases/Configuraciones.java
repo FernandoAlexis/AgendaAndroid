27902530409281403c89d48d1ca28284aa4efc8e
FernandoAlexis <62911105+FernandoAlexis@users.noreply.github.com>
2023-04-22 09:57:09 -0600

    Changing succesfull

---
+++ app/src/main/java/com/example/practica_11/clases/Configuraciones.java
@@ -0,0 +1,8 @@
+package com.example.practica_11.clases;
+
+public class Configuraciones {
+    public String urlWebServices;
+    public Configuraciones(){
+        this.urlWebServices = "http://192.168.1.9/webservices/webservices02.php";
+    }
+}
